ef7c759d8feaf173051dd741dc2e77bd1148335a
yiyinglee0408 <67095034+yiyinglee0408@users.noreply.github.com>
2022-11-12 14:50:14 +0800

    Courier Dashboard

--- courier_dashboard.php
+++ courier_dashboard.php
@@ -16,6 +16,7 @@
 			{
 				background-color:#F4F7F9;
 			}
+			
 		</style>
 		
 		<script src="//code.jquery.com/jquery-1.9.1.js"></script>
@@ -50,13 +51,28 @@
 	
 		<h1 style = "color:#21618C; margin-left:330px; padding-top:30px;">Dashboard</h1><br/>
 		
-		<div class = "container">
-			 <div style="width:50%;hieght:10%;text-align:center; margin-left:330px;">
-				<h2 class="page-header" >Statistic Analysis</h2>
-				<div>Courier Revenue</div>
-				<canvas  id="chartjs_bar"></canvas> 
-			 </div>   
-		</div>
+		 <div style="width:50%;hieght:20%;text-align:center; margin-left:330px;">
+			<h2 class="page-header" >Statistic Analysis</h2>
+			<div>Courier Revenue</div>
+			<canvas  id="chartjs_bar"></canvas> 
+		 </div><br><br>
+
+		<?php
+			$sql1 = "SELECT expenses as e, SUM(price) as p FROM courierexpenses WHERE courierexpenses.courierID='$courierID' GROUP BY expenses ORDER BY expenses";
+			$result1 = mysqli_query($combine, $sql1);
+			$chart_data1="";
+			while($row1 = mysqli_fetch_assoc($result1))
+			{
+				$price[]  = $row1['p']  ;
+				$expenses[] = $row1['e'];
+			 }
+		?>
+		
+		<div style="width:50%;hieght:20%;text-align:center; margin-left:330px;">
+            <h2 class="page-header" >Statistic Analysis</h2>
+            <div>Courier Expenses</div>
+            <canvas  id="chartjs_bar1"></canvas> 
+        </div>    
 		
 		<?php
 			$sql = "SELECT * FROM user_delivery_details WHERE courierID = '$courierID'";
@@ -85,6 +101,40 @@
 									"#ff004e"
 								],
 								data:<?php echo json_encode($totalPrice); ?>,
+							}]
+						},
+						options: {
+							   legend: {
+							display: true,
+							position: 'bottom',
+	 
+							labels: {
+								fontColor: '#71748d',
+								fontFamily: 'Circular Std Book',
+								fontSize: 14,
+							}
+						},
+	 
+	 
+					}
+					});
+			
+			 var ctx1 = document.getElementById("chartjs_bar1").getContext('2d');
+					var myChart = new Chart(ctx1, {
+						type: 'bar',
+						data: {
+							labels:<?php echo json_encode($expenses); ?>,
+							datasets: [{
+								backgroundColor: [
+								   "#5969ff",
+									"#ff407b",
+									"#25d5f2",
+									"#ffc750",
+									"#2ec551",
+									"#7040fa",
+									"#ff004e"
+								],
+								data:<?php echo json_encode($price); ?>,
 							}]
 						},
 						options: {
