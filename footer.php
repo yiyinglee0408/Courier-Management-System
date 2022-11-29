2532f90252e3d0db8e43b1da792d2802169be6a0
yiyinglee0408 <67095034+yiyinglee0408@users.noreply.github.com>
2022-11-30 00:43:54 +0800

    Homepage Footer

---
+++ footer.php
@@ -0,0 +1,23 @@
+<style>
+		/*footer*/
+		.footer {
+		  width: 100%;
+		  height:65px;
+		  background-position: center;
+          background-repeat: no-repeat;
+          background-size: cover;
+		  position: relative;
+		  display: flex;
+		  flex-wrap: nowrap;
+		  font-size:15px;
+		  background-color:#34495E;
+		}
+
+	</style>
+		
+		<br /><br/><br>
+		<div class='footer'>
+			<p style = "color:white; margin-left:20px; margin-top:25px">Copyright ©2022 Courier Management System<p>
+		</div>
+	</body>
+</html>
